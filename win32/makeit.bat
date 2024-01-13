07bc7ebc8e0edd5ea777e42bd2caee1e8c43a897
Alexey Ivanov <code@alexey-ivanov.name>
2024-01-13 21:12:50 +0000

    Merge .const and .rdata with .text
    Merge .const and .rdata sections with
    .text section which results in smaller
    .exe: only 1,024 bytes.
    
    Suppress relocation info,
    Mark as release, and
    Set version to 1.0.

--- win32/makeit.bat
+++ win32/makeit.bat
@@ -6,4 +6,7 @@ rem     /stub:..\dos\Hello.exe ^
 %MASM%\bin\ml.exe /c /Cp /coff Hello.asm && ^
 %MASM%\bin\link.exe /subsystem:console,4.0 ^
     /map:Hello.map ^
+    /merge:.const=.text ^
+    /merge:.rdata=.text ^
+    /fixed /release /version:1.0 ^
     Hello.obj %MASM%\lib\kernel32.lib
