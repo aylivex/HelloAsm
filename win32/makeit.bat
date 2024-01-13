da910f1a687cbc9df65877ca45f4777cef2126b2
Alexey Ivanov <code@alexey-ivanov.name>
2024-01-13 21:11:37 +0000

    Comment out custom dos stub in win32

--- win32/makeit.bat
+++ win32/makeit.bat
@@ -2,8 +2,8 @@
 set MASM=C:\devtools\assembler\masm32
 rem set MASM=C:\masm32
 rem /c for compile only
+rem     /stub:..\dos\Hello.exe ^
 %MASM%\bin\ml.exe /c /Cp /coff Hello.asm && ^
 %MASM%\bin\link.exe /subsystem:console,4.0 ^
     /map:Hello.map ^
-    /stub:..\dos\Hello.exe ^
     Hello.obj %MASM%\lib\kernel32.lib
