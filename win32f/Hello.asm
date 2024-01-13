ff72027cf83850dfdf1317282bc73b45e4ec08ed
Alexey Ivanov <code@alexey-ivanov.name>
2024-01-13 21:24:42 +0000

    Move imports into code section in FASM
    To produce smaller executable, declare imports
    at the start of the code section.
    Now the .exe is only 1,024 bytes.
    
    Mark the executable as DEP/NX compatible.

--- win32f/Hello.asm
+++ win32f/Hello.asm
@@ -1,35 +1,9 @@
-format PE Console 4.0
+format PE Console 4.0 NX
 entry start
 
-section '.text' code readable executable
+section '.text' code import readable executable
 
-start:
-    ; hStdOut = GetstdHandle( STD_OUTPUT_HANDLE)
-    push    -11
-    call    [GetStdHandle]
-
-    ; Reserve space for DWORD  bytes;    
-    sub     esp, 4
-    mov     ebx, esp
-    ; WriteFile( hstdOut, message, length(message), &bytes, 0);
-    push    0
-    push    ebx
-    push    messageLen
-    push    message
-    push    eax
-    call    [WriteFile]
-
-    ; ExitProcess(0)
-    push    0
-    call    [ExitProcess]
-
-
-message db 'Hello World!', 13, 10
-messageLen = $ - message
-
-
-section '.idata' import data readable writeable
-
+; Import table
   dd 0,0,0,RVA kernel_name,RVA kernel_table
   dd 0,0,0,0,0
 
@@ -46,6 +20,32 @@ _GetStdHandle   dw 0
 _WriteFile      dw 0
                 db 'WriteFile', 0
 _ExitProcess    dw 0
-                db 'ExitProcess',0
+                db 'ExitProcess', 0
+
+align 10h
+start:
+    ; hStdOut = GetstdHandle(STD_OUTPUT_HANDLE)
+    push    -11
+    call    [GetStdHandle]
+
+    ; Reserve space for DWORD  bytes;    
+    sub     esp, 4
+    mov     ebx, esp
+    ; WriteFile(hStdOut, message, length(message), &bytes, 0);
+    push    0
+    push    ebx
+    push    messageLen
+    push    message
+    push    eax
+    call    [WriteFile]
+
+    ; ExitProcess(0)
+    push    0
+    call    [ExitProcess]
+
+
+message db 'Hello World!', 13, 10
+messageLen = $ - message
+
 
 ;section '.reloc' fixups data readable discardable	; needed for Win32s
