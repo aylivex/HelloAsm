4172269b239d80d27a39918942457546e0650cf6
Alexey Ivanov <code@alexey-ivanov.name>
2024-03-03 19:19:05 +0000

    Reserve stack for without creating stack frame
    Amend win32u and modify makeit.bat

--- win32u/Hello.asm
+++ win32u/Hello.asm
@@ -16,7 +16,7 @@ message dw 'H', 'e', 'l', 'l', 'o',
            ' ', 'f', 'r', 'o', 'm',
            ' ', 'W', 'i', 'n', '3', '2',
             13,  10,
-           041Fh, 0440h, 0438h, 0432h, ; 'Привет из Win32'
+           041Fh, 0440h, 0438h, 0432h, ; 'пїЅпїЅпїЅпїЅпїЅпїЅ пїЅпїЅ Win32'
            0435h, 0442h, 0020h, 0438h,
            0437h, 0020h, 0057h, 0069h,
            006Eh, 0033h, 0032h,
@@ -26,22 +26,20 @@ messageLen = ($ - message) / 2
 .code
 
 _main:
-    ; DWORD  bytes;    
-    mov     ebp, esp
-    sub     esp, 4
-
     ; hStdOut = GetstdHandle( STD_OUTPUT_HANDLE)
     push    -11
     call    _GetStdHandle@4
-    mov     ebx, eax    
+
+    ; Reserve space for DWORD bytes
+    sub     esp, 4
+    mov     ebx, esp
 
     ; WriteFile( hstdOut, message, length(message), &bytes, 0);
     push    0
-    lea     eax, [ebp-4]
-    push    eax
+    push    ebx
     push    messageLen
     push    offset message
-    push    ebx
+    push    eax
     ;call    _WriteFile@20
     call    _WriteConsoleW@20
 
--- win32u/makeit.bat
+++ win32u/makeit.bat
@@ -1,7 +1,8 @@
 @echo off
+set MASM=C:\devtools\assembler\masm32
 rem set PATH=C:\masm32\bin
 rem /c for compile only
-C:\masm32\bin\ml.exe /c /Cp /coff Hello.asm && ^
-C:\masm32\bin\link.exe /map:Hello.map /subsystem:console ^
+%MASM%\bin\ml.exe /c /Cp /coff Hello.asm && ^
+%MASM%\bin\link.exe /map:Hello.map /subsystem:console ^
     /release ^
-    Hello.obj C:\masm32\lib\kernel32.lib
+    Hello.obj %MASM%\lib\kernel32.lib
